b077016f69e1472b4ad25e15755897e52e72b893
Unieo <unieo@aol.com>
2021-01-06 15:23:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -100,27 +100,27 @@ client.on("message", (message) => {
   } else if (command === "smack") {
     client.commands.get("smack").execute(message);
   }
-});
 
-if (!cooldowns.has(command.name)) {
-  cooldowns.set(command.name, new Discord.Collection());
-}
-
-const now = Date.now();
-const timestamps = cooldowns.get(command.name);
-const cooldownAmount = (command.cooldown || 3) * 1000;
-
-if (timestamps.has(message.author.id)) {
-  const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-  if (now < expirationTime) {
-    const timeLeft = (expirationTime - now) / 1000;
-    return message.reply(
-      `please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${
-        command.name
-      }\` command.`
-    );
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Discord.Collection());
   }
-}
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000;
+
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.reply(
+        `please wait ${timeLeft.toFixed(
+          1
+        )} more second(s) before reusing the \`${command.name}\` command.`
+      );
+    }
+  }
+});
 
 client.login(process.env.token);
