6f36f01a72a1bdc784907a4e04426779e2e9a47b
Unieo <unieo@aol.com>
2021-01-10 21:19:22 +0530

    Few Changes

--- commands/about.js
+++ commands/about.js
@@ -10,9 +10,8 @@ module.exports = {
     let credit =
       "A big thank you to my friend **Lookin** in helping me solve bugs etc.";
     let licence = "MIT";
-    let sourceCode =
-      "Click [here](https://github.com/Unieo/Delta-Canary) to go to GitHub";
-    let version = "1.0.14 Canary Release";
+    let sourceCode = `Click [here](${config.source}) to go to GitHub`;
+    let version = `${config.version}`;
     let notes =
       "**Delta:** We recommend the official Delta bot for larger communities... [click here](https://discord.com/oauth2/authorize?client_id=786882695186874368&scope=bot&permissions=2147483647) to invite the bot!\n**Delta Beta:** Like Stable Builds but Awesome Features? Or do you just want to test out new feature's before anyone else? [click here](https://discord.com/oauth2/authorize?client_id=796231105665040414&scope=bot&permissions=2147483647) to invite the Beta version of Delta!";
     const embed = new Discord.MessageEmbed()
