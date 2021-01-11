d9d5f0ebf0f129cee81eaedc73d0c02b9c354e38
Unieo <unieo@aol.com>
2021-01-11 15:12:57 +0530

    New Commands, Bug Fixes
    Added new and updated commands: birb and fox.
    Bug Fixes: Updated Version, Updated help file, deleted anime file.

--- commands/birb.js
+++ commands/birb.js
@@ -6,13 +6,14 @@ module.exports = {
   name: "fox",
   cooldown: 10,
   async execute(client, message, args) {
-    const { body } = await superagent.get("https://some-random-api.ml/meme");
+    const { body } = await superagent.get(
+      "https://some-random-api.ml/img/birb"
+    );
 
     const embed = new Discord.MessageEmbed()
       .setColor(Math.floor(Math.random() * 16777215))
       .setTitle("Here's Your Birb")
-      .setDescription(body.caption)
-      .setImage(body.image)
+      .setImage(body.link)
       .setTimestamp()
       .setFooter(`${config.copyright}`);
     message.channel.send({ embed });
--- commands/fox.js
+++ commands/fox.js
@@ -6,13 +6,12 @@ module.exports = {
   name: "fox",
   cooldown: 10,
   async execute(client, message, args) {
-    const { body } = await superagent.get("https://some-random-api.ml/meme");
+    const { body } = await superagent.get("https://some-random-api.ml/img/fox");
 
     const embed = new Discord.MessageEmbed()
       .setColor(Math.floor(Math.random() * 16777215))
-      .setTitle("Here's Your Birb")
-      .setDescription(body.caption)
-      .setImage(body.image)
+      .setTitle("Here's Your Fox")
+      .setImage(body.link)
       .setTimestamp()
       .setFooter(`${config.copyright}`);
     message.channel.send({ embed });
