5173aafe887d6079cb8df25120854b878656b4fd
Unieo <unieo@aol.com>
2021-01-12 09:45:40 +0530

    Bug Fix

--- commands/meme.js
+++ commands/meme.js
@@ -6,12 +6,12 @@ module.exports = {
   name: "meme",
   cooldown: 10,
   async execute(client, message, args) {
-    const data = await axios.get("https://some-random-api.ml/meme");
+    const { image } = await axios.get("https://some-random-api.ml/meme");
 
     const embed = new Discord.MessageEmbed()
       .setColor(Math.floor(Math.random() * 16777215))
       .setTitle("Here's Your Meme")
-      .setImage(data.image)
+      .setImage(image)
       .setTimestamp()
       .setFooter(`${config.copyright}`);
     return message.channel.send({ embed });
